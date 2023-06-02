af1c0338e8f7e92600cd84a409cbaee720c78bd0
ibrahimwichka <wikipediaofsoccer@gmail.com>
2023-06-01 20:58:53 -0400

    ejs

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express")
 const logger = require("morgan")
 const app = express()
 const port = 3000
+const DEBUG = true;
+const db = require("./db/db_connection");
+
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
 
 app.use(logger("dev"))
 app.use(express.static(__dirname + '/public'))
@@ -11,7 +16,7 @@ app.use(express.static(__dirname + '/public'))
 
 // define a route for the default homepage
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render('index')
 });
 
 app.get("/detail", (req,res) => {
@@ -26,8 +31,30 @@ app.get("/profiles", (req,res) => {
     res.sendFile(__dirname + "/views/profiles.html");
 });
 
+
+const read_workouts_all_sql = `
+    SELECT *
+    FROM Workouts
+    JOIN Muscles
+        ON Workouts.muscle_id = Muscles.muscle_id
+    ORDER BY
+        Workouts.workout_id 
+`
+
 app.get("/workouts", (req,res) => {
-    res.sendFile(__dirname + "/views/workouts.html");
+    db.execute(read_workouts_all_sql, (error, results) =>
+    {
+        if (DEBUG){
+            console.log(error ? error:results)
+        }
+        if (error){
+            res.status(500).send(error)
+        }
+        else {
+            let data = {work: results[0]}
+            res.render("workouts", data)
+        }
+    })
 });
 
 // start the server
